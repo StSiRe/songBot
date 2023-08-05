604acf5b5b97367201fbb45e1f1f735665855e72
andrew <andy_hooli@outlook.com>
2023-08-06 01:04:53 +0300

    Checking tg token

--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send("Hello")
+    res.send("ddd")
 })
 
 app.listen(8080, ()=>{
     console.log('server started')
 })
+
+console.log(process.env.TG_TOKEN);
