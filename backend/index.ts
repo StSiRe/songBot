0fe5b17b946186949330fa2a33870cf1370ac6ac
andrew <andy_hooli@outlook.com>
2023-08-05 19:20:52 +0300

    fix

--- backend/index.ts
+++ backend/index.ts
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send('<h1>HELLO WORLD</h1>')
-    res.send('You is faggot')
+
+    res.send('<h1>HELLO WORLD</h1> <div>You is faggot </div>')
 })
 
 app.listen(8080, ()=>{
