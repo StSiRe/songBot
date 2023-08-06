4d530603bb5a7a8c9b86dfdef20c8180bf3aa5c4
andrew <andy_hooli@outlook.com>
2023-08-06 13:28:18 +0300

    Checking tg token

--- backend/index.ts
+++ backend/index.ts
@@ -4,9 +4,14 @@ import { Bot } from 'grammy'
 const bot = new Bot(process.env.TG_TOKEN as string)
 
 // 2. Reply to text messages with the received text
-bot.on('message:text', ctx => ctx.reply(ctx.message.text))
+//bot.on('message:text', ctx => ctx.reply(ctx.message.text))
 
-
-bot.command('start', (ctx) => ctx.reply(ctx.from.first_name));
+bot.on("message", async (ctx) => {
+    // `txt` will be a `string` when processing text messages.
+    // It will be `undefined` if the received message does not have any message text,
+    // e.g. photos, stickers, and other messages.
+    const txt = ctx.message.text;
+});
+//bot.command('/start', (ctx) => ctx.reply(ctx.from.first_name));
 // 3. Start the bot
 bot.start()
