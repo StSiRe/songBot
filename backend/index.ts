9f32f265212d048df8db38aabc7abbe989fbfe6f
andrew <andy_hooli@outlook.com>
2023-08-06 13:32:45 +0300

    Trying to understand how to identify tg user

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,7 @@ bot.on("message", async (ctx) => {
     // `txt` will be a `string` when processing text messages.
     // It will be `undefined` if the received message does not have any message text,
     // e.g. photos, stickers, and other messages.
-    const txt = ctx.message.text;
+    const txt = ctx.from.id + " " + ctx.from.first_name + " " + ctx.from.username;
 
     await ctx.reply(txt);
 });
