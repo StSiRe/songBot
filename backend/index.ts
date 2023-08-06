69c7412ae2f2f351036cdf15399f0f5f79640e97
andrew <andy_hooli@outlook.com>
2023-08-06 13:24:29 +0300

    Checking tg token

--- backend/index.ts
+++ backend/index.ts
@@ -6,5 +6,7 @@ const bot = new Bot(process.env.TG_TOKEN as string)
 // 2. Reply to text messages with the received text
 bot.on('message:text', ctx => ctx.reply(ctx.message.text))
 
+
+bot.command('start', (ctx) => ctx.reply(ctx.from.first_name));
 // 3. Start the bot
 bot.start()
